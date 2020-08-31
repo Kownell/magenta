1341feeaf9f98bccf63a29bb6d88de37a991704a
Kownell <kownell@gmail.com>
2020-08-31 18:19:04 +0900

    stepを表示

--- magenta/models/performance_rnn/performance_sequence_generator.py
+++ magenta/models/performance_rnn/performance_sequence_generator.py
@@ -271,6 +271,8 @@ def _step_to_value(step, num_steps, values):
 def _step_to_emb(step, num_steps, values):
   """Map step in performance to desired control signal value."""
   step = min(step,num_steps-1)
+  if step % 20 == 0:
+      print('\%d' % step, end='')
   return step / num_steps # ** values
 
 def get_generator_map():
