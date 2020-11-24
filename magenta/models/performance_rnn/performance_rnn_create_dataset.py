6c5cb96c4ac58ff758a67c4d452bd1ced0c1ee62
Kownell <kownell@gmail.com>
2020-11-25 02:19:52 +0900

    loggingレベルをdebugに

--- magenta/models/performance_rnn/performance_rnn_create_dataset.py
+++ magenta/models/performance_rnn/performance_rnn_create_dataset.py
@@ -27,6 +27,7 @@ from magenta.pipelines import performance_pipeline
 from magenta.pipelines import pipeline
 import tensorflow.compat.v1 as tf
 
+tf.logging.basicConfig(level=tf.logging.DEBUG)
 flags = tf.app.flags
 FLAGS = tf.app.flags.FLAGS
 flags.DEFINE_string(
