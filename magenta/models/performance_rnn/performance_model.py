a36c998c01a50c619718de67008e8e8073456c1f
Kownell <kownell@gmail.com>
2020-11-30 21:35:58 +0900

    新しいconfig

--- magenta/models/performance_rnn/performance_model.py
+++ magenta/models/performance_rnn/performance_model.py
@@ -423,5 +423,25 @@ default_configs = {
                     time_embbeding_bin = 32
                 )],
             global_condition=GlobalConditioning(csv='/media/oeda/HDCZ-UT1/data/dataset/tags/',tags=['感情分類'])
+            ),
+    'rerative_time_and_song_length_performance_with_dynamics':
+        PerformanceRnnConfig(
+            generator_pb2.GeneratorDetails(
+                id='multiconditioned_performance_with_dynamics',
+                description='Density- and pitch-conditioned Performance RNN'),
+            note_seq.OneHotEventSequenceEncoderDecoder(
+                note_seq.PerformanceOneHotEncoding(num_velocity_bins=32)),
+            contrib_training.HParams(
+                batch_size=64,
+                rnn_layer_sizes=[512, 512, 512],
+                dropout_keep_prob=1.0,
+                clip_norm=3,
+                learning_rate=0.001),
+            num_velocity_bins=32,
+            control_signals=[
+                timepipe.RelativeTimePerformanceControlSignal(
+                    time_embbeding_bin = 32
+                )],
+            global_condition=GlobalConditioning(csv='/media/oeda/HDCZ-UT1/data/dataset/tags/',tags=['感情分類'],song_bin=32,max_length=30)
             )
 }
