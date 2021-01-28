e3944342963a414e2274b12d1991b9af481baebd
Kownell <kownell@gmail.com>
2021-01-28 15:45:48 +0900

    debug

--- magenta/models/shared/events_rnn_model.py
+++ magenta/models/shared/events_rnn_model.py
@@ -380,8 +380,8 @@ class EventSequenceRnnModel(model.BaseModel):
 
     tf.logging.info('Beam search yields sequence with log-likelihood: %f ',
                     loglik)
-    print(np.asarray(events).shape)
-    return list(reversed(events))
+    print(type(events))
+    return events
 
   def _evaluate_batch_log_likelihood(self, event_sequences, inputs,
                                      initial_state):
